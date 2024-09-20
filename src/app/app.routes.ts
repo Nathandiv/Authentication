fd264abba0d9e7d1986aed83edbd30912f05e523
Nathandiv <k.n.hlungwani@gmail.com>
2024-09-20 12:10:14 +0200

    made routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './Pages/dashboard/dashboard.component';
+import { LoginComponent } from './Pages/login/login.component';
+import { RegisterComponent } from './Pages/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'register',component:RegisterComponent},
+  {path:'login',component:LoginComponent},
+  {path:'dashboard',component:DashboardComponent},
+  {path: "**", redirectTo: "/error", pathMatch: "full"},
+  {path:'error',component:RegisterComponent}
+
+
+];
